5dd25fcc9b17da3489ffaaa08f0172aec8e20c1e
luu.hoait <luu.hoait@gmail.com>
2022-05-28 11:04:09 +0700

    backup

--- wp-content/themes/tpf-wp-team/header.php
+++ wp-content/themes/tpf-wp-team/header.php
@@ -7,7 +7,7 @@
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 
