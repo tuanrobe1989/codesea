150547d7bf2602bad71d6952d29e626e3d696f26
luu.hoait <luu.hoait@gmail.com>
2022-06-05 15:20:05 +0700

    backup

--- wp-content/themes/tpf-wp-team/page.php
+++ wp-content/themes/tpf-wp-team/page.php
@@ -1,3 +1,8 @@
 <?php get_header(); ?>
+<?php if (!is_front_page()) : ?>
+    <div class="page__tit">
+        <h1><?php the_title() ?></h1>
+    </div>
+<?php endif; ?>
 <?php the_content(); ?>
 <?php get_footer(); ?>
