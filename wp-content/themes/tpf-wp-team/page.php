fe1c061a05959519095a47ab242cdbf743139175
luu.hoait <luu.hoait@gmail.com>
2022-06-05 17:12:32 +0700

    backup

--- wp-content/themes/tpf-wp-team/page.php
+++ wp-content/themes/tpf-wp-team/page.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 <?php if (!is_front_page()) : ?>
-    <div class="page__tit">
-        <h1><?php the_title() ?></h1>
+    <div class="page__titWrap">
+        <div class="container">
+            <h1 class="page__tit"><?php the_title() ?></h1>
+        </div>
     </div>
 <?php endif; ?>
 <?php the_content(); ?>
