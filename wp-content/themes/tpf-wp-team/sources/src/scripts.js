3b68cf7699efbf6f7398a88aacfd298729afb47a
luu.hoait <luu.hoait@gmail.com>
2022-06-14 22:08:51 +0700

    backup

--- wp-content/themes/tpf-wp-team/sources/src/scripts.js
+++ wp-content/themes/tpf-wp-team/sources/src/scripts.js
@@ -19,14 +19,6 @@ tpfObjects.hamb = function () {
     })
 }
 
-tpfObjects.faqItem = function () {
-    if (jQuery('.faqItem').length > 0) {
-        jQuery('.faqItem__title').click(function () {
-            jQuery(this).closest('.faqItem').toggleClass('actived');
-        })
-    }
-
-}
 
 tpfObjects.menu = function(){
     var currentTop;
@@ -40,119 +32,34 @@ tpfObjects.menu = function(){
     })
 }
 
-
-tpfObjects.coverHbsptFormPopups = function () {
-    jQuery('.sgpb-popup-overlay').click(function () {
-        jQuery('.sgpb-popup-dialog-main-div-wrapper .hs-form-iframe').each(function () {
-            jQuery(this).removeClass('actived');
-        })
-    })
-    jQuery('.sgpb-popup-dialog-main-div-wrapper .hs-form-iframe').each(function () {
-        if (jQuery(this).hasClass('actived') == false) {
-            jQuery(this).addClass('actived');
-            var haftSectionBgForm = jQuery(this).closest('.haftSectionBgForm');
-            var head = jQuery(this).contents().find("head");
-            //var body = jQuery(this).contents().find("body");
-            if (jQuery(this).contents().find('#coverhsform').length == 0 && jQuery(this).contents().find('#coverhsform-2').length == 0) {
-                var version = Math.floor(Math.random() * 99999);
-                var css = "<link id='coverhsform' rel='stylesheet' href='" + global_params.themes_url + "/dist/css/cover_iframe.css?ver=" + version + "' id='cover_iframe' type='text/css' media='all' />";
-                var css2 = "<link id='coverhsform-2' rel='stylesheet' href='" + global_params.themes_url + "/dist/css/cover_iframe_background.css?ver=" + version + "' id='cover_iframe_bg_form' type='text/css' media='all' />";
-                // var js = "<script type='text/javascript' src='" + global_params.themes_url + "/dist/js/cover_iframe.js?ver=" + version + "' id='cover_iframe'></script>"
-                jQuery(head).append(css);
-                if (haftSectionBgForm.html() !== undefined) {
-                    jQuery(head).append(css2);
-                }
-                // jQuery(body).append(js);
+tpfObjects.goalTargets = function(){
+    jQuery('.goals__targets').owlCarousel({
+        loop:false,
+        items:1,
+        nav:true,  
+        dots: false, 
+        responsive:{
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
             }
         }
-    })
+    });
 }
 
-tpfObjects.coverHbsptForm = function () {
-    jQuery('.content .hs-form-iframe').each(function () {
-        if (jQuery(this).hasClass('actived') == false) {
-            jQuery(this).addClass('actived');
-            var haftSectionBgForm = jQuery(this).closest('.haftSectionBgForm');
-            var head = jQuery(this).contents().find("head");
-            //var body = jQuery(this).contents().find("body");
-            var version = Math.floor(Math.random() * 99999);
-            var css = "<link rel='stylesheet' href='" + global_params.themes_url + "/dist/css/cover_iframe.css?ver=" + version + "' id='cover_iframe' type='text/css' media='all' />";
-            var css2 = "<link rel='stylesheet' href='" + global_params.themes_url + "/dist/css/cover_iframe_background.css?ver=" + version + "' id='cover_iframe_bg_form' type='text/css' media='all' />";
-            // var js = "<script type='text/javascript' src='" + global_params.themes_url + "/dist/js/cover_iframe.js?ver=" + version + "' id='cover_iframe'></script>"
-            jQuery(head).append(css);
-            if (haftSectionBgForm.html() !== undefined) {
-                jQuery(head).append(css2);
-            }
-            // jQuery(body).append(js);
-        }
-    })
-}
 
-tpfObjects.tabsContent = function () {
-    if (jQuery('.tabsContent').length > 0) {
-        jQuery('.tabsContent__tab').click(function () {
-            var id = jQuery(this).data('id');
-            var tabsContents = jQuery(this).closest('.tabsContents');
-            tabsContents.find('.tabsContent__tab').each(function () {
-                jQuery(this).removeClass('actived');
-            })
-            jQuery(this).toggleClass('actived');
-            tabsContents.find('.tabsContent__content').each(function () {
-                jQuery(this).removeClass('actived');
-            })
-            tabsContents.find('#' + id).toggleClass('actived');
-        })
-    }
-}
-
-tpfObjects.coverSocials = function () {
-    if (jQuery('.facebook-reviews  .HeaderTitle__Text-sc-cxt6be-2.cOdTFM').length > 0) {
-        jQuery('.facebook-reviews .HeaderTitle__Text-sc-cxt6be-2.cOdTFM').html('<span class="h2">Facebook Reviews</span>')
-    }
-    if (jQuery('.google-reviews  .HeaderTitle__Text-sc-cxt6be-2.cOdTFM').length > 0) {
-        jQuery('.google-reviews .HeaderTitle__Text-sc-cxt6be-2.cOdTFM').html('<span class="h2">Google Reviews</span>')
-    }
-}
-
-function handler(e) {
-    e.preventDefault();
-    videotarget = this.getAttribute("href");
-    filename = videotarget.substr(0, videotarget.lastIndexOf('.')) || videotarget;
-    video = document.querySelector("#dragonball video");
-    video.removeAttribute("controls");
-    video.removeAttribute("poster");
-    source = document.querySelectorAll("#dragonball video source");
-    source[0].src = filename + ".mp4";
-    source[1].src = filename + ".webm";
-    video.load();
-    video.play();
-}
 
 jQuery(document).ready(function ($) {
     MatchHeight.init();
     tpfObjects.hamb();
     tpfObjects.menu();
-    tpfObjects.faqItem();
-    tpfObjects.tabsContent();
+    tpfObjects.goalTargets();
 
-    jQuery('.sg-popup-custom-01').on('click', function () {
-        setTimeout(function () {
-            tpfObjects.coverHbsptFormPopups();
-        }, 100);
-    })
-
-    setTimeout(function () {
-        tpfObjects.coverHbsptForm();
-    }, 800);
-    setTimeout(function () {
-        tpfObjects.coverSocials();
-    }, 500);
-
-
-    jQuery('iframe').load(function () {
-        jQuery('iframe').contents().find("head")
-            .append(jQuery("<style type='text/css'>  .label{color: red !important}  </style>"));
-    });
 
     if (jQuery('.courseGallery').length > 0) {
         $('.courseGallery').owlCarousel({
